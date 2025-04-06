cb609b3b731c3afa6bac9655cfff1989c643035c
Sourav Adhikari <ad.sourav21@live.com>
2025-04-06 14:38:01 +0000

    updated insert_data.sh

--- insert_data.sh
+++ insert_data.sh
@@ -12,7 +12,7 @@ echo $($PSQL "TRUNCATE teams, games")
 
 cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
 do
-  if [[ $YEAR != year ]]
+  if [[ $WINNER != winner ]]
   then
     # get team_id from $WINNER
     TEAM_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
@@ -40,9 +40,34 @@ do
       then
         echo Inserted into teams, $OPPONENT
       fi
-    fi  
+    fi
+  fi
+done  
+
+cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+do
+  if [[ $YEAR != year ]]
+  then
+    # get WINNER_ID from teams
+    WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+
+    # get OPPONENT_ID from teams
+    OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
 
     # get game_id
-    GAME_ID=$($PSQL "SELECT game_id FROM games WHERE (year=$YEAR AND round=$ROUND AND winner_id=$WINNER_)")
+    GAME_ID=$($PSQL "SELECT game_id FROM games WHERE (year=$YEAR AND round='$ROUND' AND winner_id=$WINNER_ID)")
+
+    # if not found
+    if [[ -z $GAME_ID ]]
+    then
+      # insert game
+      INSERT_GAME_RESULT=$($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES ($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS)")
+      if [[ $INSERT_GAME_RESULT == "INSERT 0 1" ]]
+      then
+        echo Inserted into games, $YEAR, $ROUND, $WINNER, $OPPONENT, $WINNER_GOALS, $OPPONENT_GOALS
+      fi
+    fi
   fi
 done
+
+  
