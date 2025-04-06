91fe4c55dd33a7bf366543c75ebfceec529e76a9
Sourav Adhikari <ad.sourav21@live.com>
2025-04-06 17:26:19 +0000

    updated queries.sh

--- queries.sh
+++ queries.sh
@@ -23,13 +23,13 @@ echo -e "\nMost goals scored in a single game by one team:"
 echo "$($PSQL "SELECT MAX(winner_goals) FROM games")"
 
 echo -e "\nNumber of games where the winning team scored more than two goals:"
-echo
+echo "$($PSQL "SELECT COUNT(game_id) FROM games WHERE winner_goals > 2")"
 
 echo -e "\nWinner of the 2018 tournament team name:"
 echo "$($PSQL "SELECT name FROM games INNER JOIN teams ON games.winner_id=teams.team_id WHERE year=2018 AND round='Final'")"
 
 echo -e "\nList of teams who played in the 2014 'Eighth-Final' round:"
-echo 
+echo "$($PSQL "SELECT name FROM teams JOIN games on (teams.team_id = games.winner_id OR teams.team_id = games.opponent_id) WHERE (year=2014 AND round='Eighth-Final') ORDER BY name")"
 
 echo -e "\nList of unique winning team names in the whole data set:"
 echo "$($PSQL "SELECT DISTINCT(name) FROM games LEFT JOIN teams ON games.winner_id=teams.team_id ORDER BY name")"
